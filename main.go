947fa4636163333426848da3dfbd97ec6bfa3915
Shuflduf <shuflduf@gmail.com>
2024-12-09 15:14:35 -0700

    start setting texure

--- main.go
+++ main.go
@@ -1,29 +1,53 @@
 package main
 
 import (
-	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
-	"log"
+    "image/color"
+    "log"
+    "os"
+
+    "github.com/hajimehoshi/ebiten/v2"
+    "github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
-type Game struct{}
+type Game struct {
+    testImg *ebiten.Image
+}
 
 func (g *Game) Update() error {
-	return nil
+    return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+    drawOptions := ebiten.DrawImageOptions{}
+    drawOptions.GeoM.Translate(50.0, 100.0)
+    screen.DrawImage(g.testImg, &drawOptions)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 320, 240
+    return 320, 240
 }
 
 func main() {
-	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Hello, World!")
-	if err := ebiten.RunGame(&Game{}); err != nil {
-		log.Fatal(err)
-	}
+    ebiten.SetWindowSize(640, 480)
+    ebiten.SetWindowTitle("Hello, World!")
+
+    // Load the image from a file
+    f, err := os.Open("path/to/your/image.png")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer f.Close()
+
+    img, _, err := ebitenutil.NewImageFromReader(f)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    game := &Game{
+        testImg: img,
+    }
+
+    if err := ebiten.RunGame(game); err != nil {
+        log.Fatal(err)
+    }
 }
